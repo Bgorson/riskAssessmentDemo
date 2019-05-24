52bbf307461a891b69b2db20963b45ff9a71ffab
BGorson <BGorson32@gmail.com>
2019-05-23 19:56:00 -0500

    initial router

--- src/App.js
+++ src/App.js
@@ -1,60 +1,33 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      greeting: ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
 
   render() { 
     return (  
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Enter your name: </label>
-        <input
-          id="name"
-          type="text"
-          value={this.state.name}
-          onChange={this.handleChange}
+      <div>
+<h1>Welcome to home page</h1>
+        <Route
+          exact path="/assessment"
+          render= { () =>
+            <React.Fragment>
+              <h1>Loadup assessment 1</h1>
+            </React.Fragment>}
         />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{this.state.greeting}</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>  
-  );
+        </div>
+    )
+
   }
+
 }
+
  
 export default App;
