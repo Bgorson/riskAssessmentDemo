774bb38775709b55404310afe43d330d394ae17f
BGorson <BGorson32@gmail.com>
2019-05-26 12:23:33 -0500

    basic endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const PORT = 4000;
 const mongoose = require ('mongoose')
+const TodoRoutes = express.Router();
+let Todo = require("./model")
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,6 +18,19 @@ const connection = mongoose.connection;
 connection.once('open', function(){
   console.log("MongoDB database connection established")
 })
+
+TodoRoutes.route('/').get(function(req,res){
+    Todo.find(function(err,todos){
+      if (err){
+        console.log(err)
+      }
+      else {
+        res.json(todos)
+      }
+    })
+})
+
+app.use('/todos', TodoRoutes)
 
 app.listen(PORT, () =>
   console.log('Express server is running on localhost:',PORT)
