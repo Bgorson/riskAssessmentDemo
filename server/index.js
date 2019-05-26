bb13017d6d7a7941c9efcb991a12a2c2eb86c515
BGorson <BGorson32@gmail.com>
2019-05-26 13:33:52 -0500

    fixed route

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
 const PORT = 4000;
 const mongoose = require ('mongoose')
-const TodoRoutes = express.Router();
+const todosRoutes = express.Router();
 let Todo = require("./model")
 
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+// app.use(pino);
+app.use(bodyParser.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/todos", {useNewUrlParser:true})
 const connection = mongoose.connection;
@@ -19,7 +19,7 @@ connection.once('open', function(){
   console.log("MongoDB database connection established")
 })
 
-TodoRoutes.route('/').get(function(req,res){
+ todosRoutes.route('/').get(function(req,res){
     Todo.find(function(err,todos){
       if (err){
         console.log(err)
@@ -29,8 +29,45 @@ TodoRoutes.route('/').get(function(req,res){
       }
     })
 })
+ todosRoutes.route('/:id').get(function(req,res){
+  let id = req.params.id;
+  Todo.findById(id, function(err,todos){
+      res.json(todos)
+  })
+})
+ todosRoutes.route('/add').post(function(req,res){
+  let todo = new Todo(req.body);
+  console.log(req.body)
+  todo.save().then(todo => {
+    res.status(200).json({'todo': 'todo added successfully'})
+  })
+  .catch(err=> {
+    res.status(400).send("adding new todo failed")
+  })
+});
+ todosRoutes.route('/update/:id').post(function(req,res){
+  Todo.findById(req.params.id, function(err, todo){
+    if (!todo){
+      res.status(404).send("data not found")
+    }
+    else {
+      Todo.todo_description= req.body.todo_description;
+      Todo.todo_responsible = req.body.todo_responsible;
+      Todo.todo_priority = req.body.todo_priority;
+      Todo.todo_completed =req.body.todo_completed;
 
-app.use('/todos', TodoRoutes)
+      todo.save().then(todo => {
+        res.json('todo updated')
+      })
+      .catch(err=> {
+        res.status(400).send("Update not possible")
+      })
+    }
+  })
+})
+
+
+app.use('/todos', todosRoutes)
 
 app.listen(PORT, () =>
   console.log('Express server is running on localhost:',PORT)
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import  { BrowserRouter as Router, Route, Link} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import TodosList from "./components/todos-list"
 import EditTodo from "./components/edit-todos"
-import CreatTodo from "./components/create-todo"
+import CreateTodo from "./components/create-todo"
 
 class App extends Component {
 
@@ -17,7 +17,7 @@ class App extends Component {
         </div>
         <Route path = "/" exact component = {TodosList} />
         <Route path = "/edit/:id" component = {EditTodo} />
-        <Route path = "/create" component = {CreatTodo}/>
+        <Route path = "/create" component = {CreateTodo}/>
         </Router>
     )
   }
