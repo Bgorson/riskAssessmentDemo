693f379cf995dc0da90f99cf3b7f56bedd94dfdf
BGorson <BGorson32@gmail.com>
2019-06-14 16:33:19 -0500

    added heroku support

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const mongoose = require('mongoose')
 const residentRoute = express.Router();
 let Resident = require("./model")
-
+const path = require('path');
 
 
 const app = express();
@@ -13,8 +13,32 @@ app.use(bodyParser.urlencoded({
 }));
 // app.use(pino);
 app.use(bodyParser.json())
-
-mongoose.connect("mongodb://127.0.0.1:27017/Resident", {
+// Serve static files from the React app
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+if (process.env.NODE_ENV == 'production'){
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  });
+  }
+  else {
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    })
+  }
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+if (process.env.NODE_ENV == 'production'){
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
+}
+else {
+	app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	})
+}
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://127.0.0.1:27017/Resident", {
   useNewUrlParser: true
 })
 const connection = mongoose.connection;
